3a71092ea3164a13428781cc88a501a6170ec5ca
Have Samuel <samiehave9@gmail.com>
2022-12-05 19:30:09 +0300

    Add backdrop handler and styling

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mainList = document.getElementById('add-modal');
 const thankYouButton = document.querySelector('.main-data button');
 // const listItemElements = mainList.querySelectorAll('li');
+const backdrop = document.getElementById('backdrop');
+
+const backdropClickHandler = () => {
+  backdrop.classList.toggle('visible');
+};
 
 const toggleMainList = () => {
   mainList.classList.toggle('visible');
