b0c6c136d67347b34474250dc093e02aab0156da
Have Samuel <samiehave9@gmail.com>
2022-12-05 18:11:33 +0300

    Create the button variable

---
+++ app.js
@@ -0,0 +1,7 @@
+const mainList = document.getElementById('numbers');
+const thankYouBtn = 
+// const listItemElements = mainList.querySelectorAll('li');
+
+for (const listItemEl of listItemElements) {
+  console.log(listItemEl);
+}
